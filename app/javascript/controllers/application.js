7b8c04f3061d6bcc0092199e46530773acb6618e
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-10-20 10:50:55 +0200

    stimulus notification added

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+import Notification from 'stimulus-notification'
 
 const application = Application.start()
+application.register('notification', Notification)
 
 // Configure Stimulus development experience
 application.debug = false
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,9 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.1.0/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "stimulus-notification", to: "https://ga.jspm.io/npm:stimulus-notification@2.1.0/dist/stimulus-notification.es.js"
+pin "hotkeys-js", to: "https://ga.jspm.io/npm:hotkeys-js@3.10.0/dist/hotkeys.esm.js"
+pin "stimulus-use", to: "https://ga.jspm.io/npm:stimulus-use@0.50.0/dist/index.js"
