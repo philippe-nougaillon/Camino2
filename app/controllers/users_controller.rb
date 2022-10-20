0a93a43418a1c7416fb3e91a980ee11c9f1161d7
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-10-20 12:06:09 +0200

    Devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :username, :picturelink)
+      params.require(:user).permit(:name, :username, :picturelink, :email, :password, :password_confirmation)
     end
 end
