48546ac35ba3b23b97aac80abc90cb0bce391d5a
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-10-20 10:23:20 +0200

    langage changé

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module CaminoV2
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.time_zone = "Paris"
+    config.i18n.default_locale = :fr
   end
 end
