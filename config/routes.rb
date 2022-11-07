31514e86d5c2880f1aa951e8dc290a1fdc7fb547
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-11-07 12:46:57 +0100

    user : delete path removed / design improved

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  resources :users, except: [:destroy]
   
   get "sitemap" => "sitemap#show", format: :xml, as: :sitemap
 
