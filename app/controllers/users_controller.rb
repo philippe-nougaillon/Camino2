b6d9763afc81ea132fb72d74e0308add0d8682ed
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-11-09 15:39:58 +0100

    avatar readded

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,6 +72,6 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:name, :username, :picturelink, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :username, :picturelink, :email, :password, :password_confirmation, :avatar)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_102628) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_09_135820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,9 +43,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_102628) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.string "checksum"
     t.datetime "created_at", null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "audits", force: :cascade do |t|
@@ -238,4 +245,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_102628) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
