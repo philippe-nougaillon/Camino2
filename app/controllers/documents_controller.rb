6a97b8c974d75309f2c357c82fb2b48128933463
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-11-10 15:19:00 +0100

    file accept only / document design/search

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,16 +1,15 @@
 class DocumentsController < ApplicationController
   def index
-    @user = current_user
-    @projects = @user.projects
-    @logs = @user.logs.documents.limit(50)
-    todos_id = @user.account.todos.pluck(:id)
+    @logs = current_user.logs.documents.limit(50)
+    todos_id = current_user.account.todos.pluck(:id)
     @documents = ActiveStorage::Attachment.where(record_type: "Todo")
                                           .where(record_id: todos_id)
                                           .order('created_at DESC')
 
     return if params[:search].blank?
 
-    @documents = @documents.where('docname ILIKE ?', "%#{params[:search]}%")
-    @logs = @logs.where('logs.description ILIKE ?', "%#{params[:search]}%")
+    blobs_id = ActiveStorage::Blob.where("filename ILIKE ?", "%#{params[:search]}%").pluck(:id)
+    @documents = @documents.where(blob_id: blobs_id)
+    # @logs = @logs.where('logs.description ILIKE ?', "%#{params[:search]}%")
   end
 end
