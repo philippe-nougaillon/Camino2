788c325a51a736c05f458b499ecb6788ca2dbdcf
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-11-14 12:23:22 +0100

    agenda week vue

--- app/controllers/agenda_controller.rb
+++ app/controllers/agenda_controller.rb
@@ -8,13 +8,8 @@ class AgendaController < ApplicationController
   end
 
   def index
-    @user = current_user
-    @projects = @user.projects.where('duedate is not null')
-    @todos = @user.todos.where('duedate is not null')
+    @projects = current_user.projects.where('duedate is not null')
+    @todos = current_user.todos.where('duedate is not null')
     @projects = @projects.where('name ILIKE ?', "%#{params[:search]}%") unless params[:search].blank?
-    respond_to do |format|
-      format.html.none
-      format.html.phone
-    end
   end
 end
