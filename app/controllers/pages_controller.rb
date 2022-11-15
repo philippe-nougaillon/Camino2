d2fe6171ef480b0b8024b1c15efe6c072945088d
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-11-15 16:17:10 +0100

    dashboard part3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,11 @@ class PagesController < ApplicationController
     @todos = current_user.account.todos
     @projects = @projects.where('name ILIKE ?', "%#{params[:search]}%") unless params[:search].blank?
 
+    @projects_start = current_user.account.projects.minimum(:created_at).to_date
+    @projects_end = current_user.account.projects.maximum(:duedate)
+    @todos_overdue = current_user.account.todos.undone.where("todos.duedate < ?", Date.today).count
+    @last_commit_picture = current_user.account.todos.done.order(:updated_at).last.user.avatar
+
     @results = current_user.account.todos.done.group("DATE(todos.updated_at)").count(:id)
   end
 end
