1a4eaa2fd8aaa3ce07f4067cc6bce624d286bbb8
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-12-07 16:54:02 +0100

    v2.3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def set_layout_variables
     @sitename ||= 'Camino'
-    @sitename.concat(' v2.2')
+    @sitename.concat(' v2.3')
     @ctrl = params[:controller]
   end
 
