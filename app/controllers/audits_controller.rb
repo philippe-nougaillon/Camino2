b139fa511457d20a98b5bd3a10367da76b4207e3
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-12-08 14:36:52 +0100

    audit securised

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -1,18 +1,23 @@
 class AuditsController < ApplicationController
+  before_action :user_authorized?
+
   def index
-    @audits  = Audited::Audit.order('id DESC')
+    @audits  = Audited::Audit.where(user_id: current_user.id).order('id DESC')
     @actions = @audits.pluck(:action).uniq.sort
     @types   = @audits.pluck(:auditable_type).uniq.sort
-    @users   = User.all
 
     @audits = @audits.where('DATE(created_at) = ?', params[:date]) unless params[:date].blank?
-
     @audits = @audits.where(action: params[:audit_action]) unless params[:audit_action].blank?
-
     @audits = @audits.where(auditable_type: params[:type]) unless params[:type].blank?
 
-    return if params[:user_id].blank?
+    # return if params[:user_id].blank?
 
-    @audits = @audits.where(user_id: params[:user_id])
+    # @audits = @audits.where(user_id: params[:user_id])
+  end
+
+  private
+
+  def user_authorized?
+    authorize :audit
   end
 end
