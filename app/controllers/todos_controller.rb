db3fc7809fc33c0cb9428c83b4cd7483ba92f335
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-12-15 13:29:53 +0100

    todo notify rake

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -233,16 +233,8 @@ class TodosController < ApplicationController
   end
 
   def todo_notifier
-    todos = current_user.account.todos
-    todos_ids = todos.where.not(duedate: nil).select { |todo| (Date.today + todo.notifydays.days) == todo.duedate }
-    todos = todos.where(id: todos_ids.pluck(:id))
-    todos.each do |todo|
-      mailer_response = Notifier.todo_notifier(todo).deliver_now
-      MailLog.create(message_id:mailer_response.message_id, to:todo.user.email, subject: "Tâche en approche.")
-    end
-
-    redirect_to user_path(current_user), notice: "TEST envoyée aux testeurs."
-
+    NotifyUsers.new(current_user.account.todos).call
+    redirect_to user_path(current_user), notice: "Emails des tâches à notifier envoyés"
   end
 
   private
---
+++ app/services/notify_users.rb
@@ -0,0 +1,16 @@
+class NotifyUsers < ApplicationService
+  attr_reader :todos
+
+  def initialize(todos)
+    @todos = todos
+  end
+
+  def call
+    todos_ids = @todos.where.not(duedate: nil).select { |todo| (Date.today + todo.notifydays.days) == todo.duedate }
+    @todos = @todos.where(id: todos_ids.pluck(:id))
+    @todos.each do |todo|
+      mailer_response = Notifier.todo_notifier(todo).deliver_now
+      MailLog.create(message_id: mailer_response.message_id, to: todo.user.email, subject: "Tâche en approche.")
+    end
+  end
+end
