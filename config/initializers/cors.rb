21c35d950a6a631e24030cf07f770543e8d32c9b
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2023-01-19 13:54:51 +0100

    local ip added

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'portfolio-philnoug.herokuapp.com', 'www.philnoug.com'
+      origins 'portfolio-philnoug.herokuapp.com', 
+              'www.philnoug.com',
+              '45.156.231.214'
       resource '/about', headers: :any, methods: :get
     end
 end
