a768953369f35b103070f0312b05fe483005ddf3
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2023-02-13 11:29:49 +0100

    v2.7 : user role editable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -106,7 +106,7 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:name, :username, :picturelink, :email, :password, :password_confirmation, :avatar)
+    params.require(:user).permit(:name, :username, :picturelink, :email, :password, :password_confirmation, :avatar, :role)
   end
 
   def user_authorized?
--- config/initializers/page_title.rb
+++ config/initializers/page_title.rb
@@ -1,1 +1,1 @@
-PageTitleHelper.options[:app] = 'Camino v2.6.a'
+PageTitleHelper.options[:app] = 'Camino v2.7'
