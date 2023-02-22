bc4b4ffdb7704a94267a0c55b69598824479a2f3
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2023-02-22 16:56:38 +0100

    api condition fixed

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -5,11 +5,12 @@ module Api
 
       def index
         todos = Account.find_by(slug: params[:slug]).todos
-        unless params[:duedate] == "null"
+
+        unless params[:duedate] == "null" || params[:duedate].blank?
           todos = todos.where(duedate: params[:duedate].to_date)
         end
 
-        unless params[:id].blank?
+        unless params[:id] == "null" || params[:id].blank?
           todos = todos.where(id: params[:id])
         end
 
