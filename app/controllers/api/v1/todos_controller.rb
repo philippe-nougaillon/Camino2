a434ee31ab37f9fb9751065c6cf88c39e7178d7d
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2023-02-24 15:07:17 +0100

    todo show first part

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -3,18 +3,36 @@ module Api
     class Api::V1::TodosController < ActionController::Base
       skip_before_action :verify_authenticity_token
 
+      before_action :set_todo, only: %i[show]
+
       def index
-        if params[:id] != "null"
-          todos = Todo.where(id: params[:id])
-        else
-          todos = Account.find_by(slug: params[:slug]).todos
-          if params[:duedate] != "null"
-            todos = todos.where(duedate: params[:duedate].to_date)
+        if !params[:slug].blank? && params[:slug] != "null"
+          data = Account.find_by(slug: params[:slug]).todos
+          if !params[:duedate].blank? && params[:duedate] != "null"
+            data = data.where(duedate: params[:duedate].to_date)
           end
+        elsif !params[:todo_slug].blank? && params[:todo_slug] != "null"
+          todo = Todo.find_by(slug: params[:todo_slug])
+          todolist = todo.todolist
+          project = todolist.project
+
+          data = todo.to_json + todolist.to_json + project.to_json
+        else
+          data = nil
         end
 
-        render json: {data: todos}
+        render json: {data: data}
       end
+
+      # def show
+      #   todo = Todo.find_by(slug: params[:todo_slug])
+      #   todolist = todo.todolist
+      #   project = todolist.project
+
+      #   data = todo.to_json + todolist.to_json + project.to_json
+
+      #   render json: {data: data}
+      # end
     end
   end
 end
