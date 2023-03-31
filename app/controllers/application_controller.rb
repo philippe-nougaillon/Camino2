24f6e78ab8ecf29c030e11df012c8ee94ff49f24
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2023-03-31 10:36:21 +0200

    theme color first part

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
 
   def set_layout_variables
     @ctrl = params[:controller]
+    @themes = ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"]
   end
 
   def detect_device_format
