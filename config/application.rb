9fbd36efc76e113235caae1e3d090b6c45654f9e
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2023-10-30 12:33:09 +0100

    try fix mailgun

--- config/application.rb
+++ config/application.rb
@@ -39,11 +39,11 @@ module CaminoV2
 
     config.active_record.use_yaml_unsafe_load = true
 
+    ActionMailer::Base.delivery_method = :mailgun
     config.action_mailer.mailgun_settings = {
       :api_key => ENV['MAILGUN_API_KEY'],
       :domain => ENV['MAILGUN_DOMAIN'],
       :api_host => 'api.eu.mailgun.net'  # Uncomment this line for EU region domains
     }
-    ActionMailer::Base.delivery_method = :mailgun
   end
 end
